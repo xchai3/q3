71b8d47bd94a27b5d342933c7cf8098292ea83cc
xchai3 <xipengc@andrew.cmu.edu>
2021-02-20 15:12:43 -0800

    again

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 4000
+
+let sendEvil = (req, res) => {
+    res.sendFile('evil.html', { root: __dirname })
+}
+
+app.get('/', sendEvil)
+
+app.get('/evil.html', sendEvil)
+
+app.listen(port, () => {
+    console.log(`evil.local server listening on port ${port}!`)
+})
